575c04830b0e41ecdae427b9cf15023bf45289fd
Nat10k <13521172@std.stei.itb.ac.id>
2024-05-22 00:20:10 +0700

    fix : walking animation (?)

--- Assets/Script/Cicak/WalkAnim.cs
+++ Assets/Script/Cicak/WalkAnim.cs
@@ -1,148 +1,111 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
-using UnityEngine.InputSystem;
 
 public class WalkAnim : MonoBehaviour
 {
-    [SerializeField] List<TwoBoneIKConstraint> leftConstraint, rightConstraint;
-    [SerializeField] Transform tailTarget;
-    [SerializeField] List<Transform> raySrcs;
-    List<Transform> targets;
-    List<Vector3> initPosition;
-    List<Quaternion> initRotation;
-    Vector3 initTailPos;
+    [SerializeField] List<Transform> legEnds, targets;
+    //[SerializeField] Transform tailTarget;
+    //[SerializeField] List<Transform> raySrcs, targetAreaPointer;
+    List<Tuple<float, bool>> legTimers;
+    //List<Vector3> initPosition;
+    //List<Quaternion> initRotation;
+    //Vector3 initTailPos;
     float timer;
+    //const float moveTargetMultiplier = 5f;
 
     private void Awake()
     {
-        initTailPos = tailTarget.localPosition;
-        targets = new List<Transform>(); initPosition = new List<Vector3>(); initRotation = new List<Quaternion>();
-        foreach (TwoBoneIKConstraint c in leftConstraint)
+        //initTailPos = tailTarget.localPosition;
+        //targets = new List<Transform>(); // initPosition = new List<Vector3>(); initRotation = new List<Quaternion>();
+        legTimers = new List<Tuple<float, bool>>
         {
-            targets.Add(c.data.target);
-            initPosition.Add(c.data.target.localPosition);
-            initRotation.Add(c.data.target.localRotation);
-        }
-        foreach (TwoBoneIKConstraint c in rightConstraint)
-        {
-            targets.Add(c.data.target);
-            initPosition.Add(c.data.target.localPosition);
-            initRotation.Add(c.data.target.localRotation);
-        }
-        foreach(Transform t in targets)
-        {
-            Debug.Log(t.gameObject.name);
-        }
+            new Tuple<float, bool>(0f, false), // Front left
+            new Tuple<float, bool>(0.1f, false), // Front right
+            new Tuple<float, bool>(0.1f, false), // Back left
+            new Tuple<float, bool>(0f, false) // Back right
+        };
         timer = 0;
-        StartCoroutine(WagTail());
+        //StartCoroutine(WagTail());
     }
 
-    private void OnEnable()
-    {
-        InputHandler.inputs.Player.Move.performed += MoveTargets;
-        InputHandler.inputs.Player.Move.canceled += ResetTargets;
-    }
+    //private void OnEnable()
+    //{
+    //    InputHandler.inputs.Player.Move.performed += MoveTargets;
+    //}
 
-    private void OnDisable()
-    {
-        InputHandler.inputs.Player.Move.performed -= MoveTargets;
-        InputHandler.inputs.Player.Move.canceled -= ResetTargets;
-    }
+    //private void OnDisable()
+    //{
+    //    InputHandler.inputs.Player.Move.performed -= MoveTargets;
+    //}
 
-    void MoveTargets(InputAction.CallbackContext ctx)
-    {
-        for (int i = 0; i < targets.Count; i++)
-        {
-            Vector2 readVal = ctx.ReadValue<Vector2>();
-            float multiplier = 8;
-            if (readVal.y != 0)
-            {
-                multiplier *= readVal.y;
-            }
-            targets[i].localPosition += Vector3.forward * multiplier;
-        }
-    }
+    //void MoveTargets(InputAction.CallbackContext ctx)
+    //{
+    //    for (int i = 0; i < targets.Count; i++)
+    //    {
+    //        Vector2 readVal = ctx.ReadValue<Vector2>();
+    //        float multiplier = 8;
+    //        if (readVal.y != 0)
+    //        {
+    //            multiplier *= readVal.y;
+    //        }
+    //        targetAreaPointer[i].localPosition += Vector3.forward * multiplier;
+    //    }
+    //}
 
-    void ResetTargets(InputAction.CallbackContext ctx)
-    {
-        for (int i = 0; i < targets.Count; i++)
-        {
-            targets[i].localPosition = initPosition[i];
-            targets[i].localRotation = initRotation[i];
-        }
-    }
+    //void ResetTargets(InputAction.CallbackContext ctx)
+    //{
+    //    for (int i = 0; i < targets.Count; i++)
+    //    {
+    //        targets[i].localPosition = initPosition[i];
+    //        targets[i].localRotation = initRotation[i];
+    //    }
+    //}
 
-    private IEnumerator WagTail()
-    {
-        float tailTimer = 0;
-        while (true)
-        {
-            while(tailTimer < 1)
-            {
-                tailTarget.localPosition = initTailPos + tailTimer * Vector3.right;
-                tailTimer += Time.deltaTime;
-                yield return null;
-            }
-            while (tailTimer > -1)
-            {
-                tailTarget.localPosition = initTailPos + tailTimer * Vector3.right;
-                tailTimer -= Time.deltaTime;
-                yield return null;
-            }
-        }
-    }
+    //private IEnumerator WagTail()
+    //{
+    //    float tailTimer = 0;
+    //    while (true)
+    //    {
+    //        while(tailTimer < 1)
+    //        {
+    //            tailTarget.localPosition = initTailPos + tailTimer * Vector3.right;
+    //            tailTimer += Time.deltaTime;
+    //            yield return null;
+    //        }
+    //        while (tailTimer > -1)
+    //        {
+    //            tailTarget.localPosition = initTailPos + tailTimer * Vector3.right;
+    //            tailTimer -= Time.deltaTime;
+    //            yield return null;
+    //        }
+    //    }
+    //}
 
     void Update()
     {
-        Vector2 moveInput = InputHandler.inputs.Player.Move.ReadValue<Vector2>();
         RaycastHit hit;
-        for (int i=0; i<raySrcs.Count; i++)
+        timer += Time.deltaTime;
+        if (timer >= 0.2f)
         {
-            if (Physics.Raycast(raySrcs[i].position, raySrcs[i].forward, out hit, 0.06f) ||
-                Physics.Raycast(raySrcs[i].position, -raySrcs[i].forward, out hit, 0.06f) ||
-                Physics.Raycast(raySrcs[i].position, raySrcs[i].right, out hit, 0.06f) ||
-                Physics.Raycast(raySrcs[i].position, -raySrcs[i].right, out hit, 0.06f))
+            timer = 0;
+            for (int i = 0; i < legTimers.Count; i++)
             {
-                targets[i].position = hit.point;
-                targets[i].right = hit.normal;
+                legTimers[i] = new Tuple<float, bool>(legTimers[i].Item1, false);
             }
         }
-        if (moveInput != Vector2.zero)
+        for (int i=0; i<legTimers.Count; i++)
         {
-            if (timer < 1)
+            if (timer >= legTimers[i].Item1)
             {
-                timer += Time.deltaTime;
-            } else
-            {
-                timer = 0;
-            }
-            if (timer < 0.5f)
-            {
-                foreach(var lCon in leftConstraint)
+                if (!legTimers[i].Item2)
                 {
-                    lCon.data.targetPositionWeight = timer / 0.5f;
-                }
-                foreach(var rCon in rightConstraint)
-                {
-                    if (rCon.data.targetPositionWeight > 0)
+                    legTimers[i] = new Tuple<float, bool>(legTimers[i].Item1, true);
+                    if (Physics.Raycast(legEnds[i].position, -transform.up, out hit, 0.1f))
                     {
-                        rCon.data.targetPositionWeight -= Time.deltaTime;
+                        targets[i].position = hit.point;
                     }
-                }
-            } else
-            {
-                foreach (var lCon in leftConstraint)
-                {
-                    if (lCon.data.targetPositionWeight > 0)
-                    {
-                        lCon.data.targetPositionWeight -= Time.deltaTime;
-                    }
-                }
-                foreach (var rCon in rightConstraint)
-                {
-                    rCon.data.targetPositionWeight = (timer-0.5f)/0.5f;
                 }
             }
         }
